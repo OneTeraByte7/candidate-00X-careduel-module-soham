5195544ff79adaa155a12d4240cc8a145eb7bab8
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2025-06-09 20:53:20 +0530

    DONE

--- client/src/components/ProposeTopicForm.jsx
+++ client/src/components/ProposeTopicForm.jsx
@@ -12,7 +12,7 @@ export default function ProposeTopicForm() {
     setMessage(null);
 
     try {
-      const res = await fetch('http://localhost:5000/api/suggest-topic', {
+      const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/suggest-topic`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ topic, userEmail }),
